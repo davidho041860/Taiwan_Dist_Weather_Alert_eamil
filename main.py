02fe2fb1d61d7f50383bab7db3938a5c1f453edb
Jingobell7418 <davidho041860@gmail.com>
2023-03-07 23:55:07 +0800

    adding name == main and reset the parameter

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def grab_weather_details( total_weather_condition, total_temperature):
     str_weather = ""
     str_weather += f"最低溫\U00002744: {min(cold_temperature_count)} | 最高溫\U0001F525: {max(cold_temperature_count)}"
     str_weather += "\n"
-    return weather_list, str_weather ,bring_umbrella
+    return weather_list, str_weather, bring_umbrella
 
 def  store_weather_detail(weather_list, Bring_umbrella, str_weather):
     if Bring_umbrella == True:
@@ -38,69 +38,70 @@ def  store_weather_detail(weather_list, Bring_umbrella, str_weather):
             str_weather += str(weather_list[i])
             str_weather += "\n"
     return str_weather
+if __name__ == "__main__":
 
-#Sender email and google apps password
-my_email = "davidho041860@gmail.com"
-password = "hqcyrpugcskwowmo"
+    #Sender email and google apps password
+    my_email = "davidho041860@gmail.com"
+    password = "hqcyrpugcskwowmo"
 
-#You can enter multiple mail receiver
-receiever = ["davidho041860@gmail.com"]
+    #You can enter multiple mail receiver
+    receiever = ["davidho041860@gmail.com"]
 
-#Generate tomorrow date
-today = datetime.now()
-tomorrow = today + timedelta(1)
-print("Today Date :", today.strftime('%Y-%m-%d'))
-print("Tomorrow Date :", tomorrow.strftime('%Y-%m-%d'))
-tomorrow_date = tomorrow.strftime('%Y-%m-%d')
+    #Generate tomorrow date
+    today = datetime.now()
+    tomorrow = today + timedelta(1)
+    print("Today Date :", today.strftime('%Y-%m-%d'))
+    print("Tomorrow Date :", tomorrow.strftime('%Y-%m-%d'))
+    tomorrow_date = tomorrow.strftime('%Y-%m-%d')
 
-#Taiwan weather api address
-OWM_Endpoint_NewTaipei_district = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-069"
-OWM_Endpoint_city = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089"
+    #Taiwan weather api address
+    OWM_Endpoint_NewTaipei_district = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-069"
+    OWM_Endpoint_city = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089"
 
-weather_params = {
-    "Authorization" : "CWB-F9B11217-1E1A-4927-9491-A1D14EA5B3A1",
-    "format" : "JSON",
-}
+    weather_params = {
+        "Authorization" : "CWB-F9B11217-1E1A-4927-9491-A1D14EA5B3A1",
+        "format" : "JSON",
+    }
 
-#Request NewTaipei district weather
-response_NewTaipei_dis = requests.get(OWM_Endpoint_NewTaipei_district, params=weather_params)
-response_NewTaipei_j_dis = response_NewTaipei_dis.json()
+    #Request NewTaipei district weather
+    response_NewTaipei_dis = requests.get(OWM_Endpoint_NewTaipei_district, params=weather_params)
+    response_NewTaipei_j_dis = response_NewTaipei_dis.json()
 
-#Request Taiwan city weather
-response_City = requests.get(OWM_Endpoint_city, params=weather_params)
-response_City_j = response_City.json()
+    #Request Taiwan city weather
+    response_City = requests.get(OWM_Endpoint_city, params=weather_params)
+    response_City_j = response_City.json()
 
-#Enter district code for specfic district
-district_code = 6
-weather_HSINTIEN = response_NewTaipei_j_dis["records"]["locations"][0]["location"][district_code]["weatherElement"]
-HSINTIEN_total_weather_condition = weather_HSINTIEN[1]["time"]
-HSINTIEN_total_temperature = weather_HSINTIEN[2]["time"]
+    #Enter district code for specfic district
+    district_code = 6
+    weather_district = response_NewTaipei_j_dis["records"]["locations"][0]["location"][district_code]["weatherElement"]
+    District_total_weather_condition = weather_district[1]["time"]
+    District_total_temperature = weather_district[2]["time"]
 
-#Enter city code for specfic district
-city_code = 9
-weather_Taipei = response_City_j["records"]["locations"][0]["location"][district_code]["weatherElement"]
-Taipei_total_weather_condition = weather_Taipei[1]["time"]
-Taipei_total_temperature = weather_Taipei[2]["time"]
+    #Enter city code for specfic district
+    city_code = 9
+    City_weather = response_City_j["records"]["locations"][0]["location"][district_code]["weatherElement"]
+    City_total_weather_condition = City_weather[1]["time"]
+    City_total_temperature = City_weather[2]["time"]
 
-HSINTIEN_weather_list, HSINTIEN_str_weather, HSINTIEN_Bring_umbrella = grab_weather_details( HSINTIEN_total_weather_condition, HSINTIEN_total_temperature)
-Taipei_weather_list, Taipei_str_weather, Taipei_Bring_umbrella = grab_weather_details( Taipei_total_weather_condition, Taipei_total_temperature)
+    District_weather_list, District_str_weather, District_Bring_umbrella = grab_weather_details( District_total_weather_condition, District_total_temperature)
+    City_weather_list, City_str_weather, City_Bring_umbrella = grab_weather_details( City_total_weather_condition, City_total_temperature)
 
-HSINTIEN_str_weather = store_weather_detail(HSINTIEN_weather_list, HSINTIEN_Bring_umbrella, HSINTIEN_str_weather)
-Taipei_str_weather = store_weather_detail(Taipei_weather_list, Taipei_Bring_umbrella, Taipei_str_weather)
+    District_str_weather = store_weather_detail(District_weather_list, District_Bring_umbrella, District_str_weather)
+    City_str_weather = store_weather_detail(City_weather_list, City_Bring_umbrella, City_str_weather)
 
-print(f"{response_NewTaipei_j_dis['records']['locations'][0]['location'][district_code]['locationName']}天氣預報\n{HSINTIEN_str_weather}")
-print(f"{response_City_j['records']['locations'][0]['location'][city_code]['locationName']}天氣預報\n{Taipei_str_weather}")
+    print(f"{response_NewTaipei_j_dis['records']['locations'][0]['location'][district_code]['locationName']}天氣預報\n{District_str_weather}")
+    print(f"{response_City_j['records']['locations'][0]['location'][city_code]['locationName']}天氣預報\n{City_str_weather}")
 
-m = EmailMessage()
+    m = EmailMessage()
 
-m['X-Priority'] = '1'
-m['Subject'] = "明日天氣預報"
-m.set_content(f"Hi,\n\n{response_NewTaipei_j_dis['records']['locations'][0]['location'][district_code]['locationName']}天氣預報\n{HSINTIEN_str_weather}\n\n{response_City_j['records']['locations'][0]['location'][city_code]['locationName']}天氣預報\n{Taipei_str_weather}")
+    m['X-Priority'] = '1'
+    m['Subject'] = "明日天氣預報"
+    m.set_content(f"Hi,\n\n{response_NewTaipei_j_dis['records']['locations'][0]['location'][district_code]['locationName']}天氣預報\n{District_str_weather}\n\n{response_City_j['records']['locations'][0]['location'][city_code]['locationName']}天氣預報\n{City_str_weather}")
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()  ##Make secure
-    connection.login(user=my_email, password=password)
-    connection.sendmail(my_email,
-                        receiever,
-                        m.as_string())
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()  ##Make secure
+        connection.login(user=my_email, password=password)
+        connection.sendmail(my_email,
+                            receiever,
+                            m.as_string())
 
