5f5670b860fe9e5a090219781f53d43b860185ed
Jingobell7418 <davidho041860@gmail.com>
2023-03-08 15:00:39 +0800

    Clean the code, add district code in NewTaipei city and city code in Taiwan

--- main.py
+++ main.py
@@ -40,21 +40,30 @@ def  store_weather_detail(weather_list, Bring_umbrella, str_weather):
     return str_weather
 if __name__ == "__main__":
 
-    #Sender email and google apps password
+    # Sender email and google apps password
     my_email = "davidho041860@gmail.com"
     password = "hqcyrpugcskwowmo"
 
-    #You can enter multiple mail receiver
+    # Enter district code for specific district and city code
+    # 0-瑞芳 1-三重 2-平溪 3-淡水 4-石門 5-泰山 6-新店 7-萬里 8-蘆洲 9-永和 10-貢寮
+    # 11-深坑 12-鶯歌 13-坪林 14-板橋 15-八里 16-土城 17-三芝 18-汐止 19-新莊 20-金山
+    # 21-林口 22-中和 23-雙溪 24-五股 25-三峽 26-樹林 27-烏來 28-石碇
+    district_code = 6
+    # 0-新竹縣 1-金門縣 2-苗栗縣 3-新北市 4-宜蘭縣 5-雲林縣 6-台南市 7-高雄市 8-彰化縣 9-台北市 10-南投縣
+    # 11-澎湖縣 12-基隆市 13-桃園市 14-花蓮縣 15-連江縣 16-台東縣 17-嘉義市 19-嘉義縣 19-屏東縣 20-台中市 21-新竹市
+    city_code = 9
+
+    # You can enter multiple mail receiver
     receiever = ["davidho041860@gmail.com"]
 
-    #Generate tomorrow date
+    # Generate tomorrow date
     today = datetime.now()
     tomorrow = today + timedelta(1)
     print("Today Date :", today.strftime('%Y-%m-%d'))
     print("Tomorrow Date :", tomorrow.strftime('%Y-%m-%d'))
     tomorrow_date = tomorrow.strftime('%Y-%m-%d')
 
-    #Taiwan weather api address
+    # Taiwan weather api address
     OWM_Endpoint_NewTaipei_district = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-069"
     OWM_Endpoint_city = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089"
 
@@ -63,43 +72,42 @@ if __name__ == "__main__":
         "format" : "JSON",
     }
 
-    #Request NewTaipei district weather
+    # Request NewTaipei district weather
     response_NewTaipei_dis = requests.get(OWM_Endpoint_NewTaipei_district, params=weather_params)
     response_NewTaipei_j_dis = response_NewTaipei_dis.json()
 
-    #Request Taiwan city weather
+    # Request Taiwan city weather
     response_City = requests.get(OWM_Endpoint_city, params=weather_params)
     response_City_j = response_City.json()
 
-    #Enter district code for specfic district
-    district_code = 6
     weather_district = response_NewTaipei_j_dis["records"]["locations"][0]["location"][district_code]["weatherElement"]
     District_total_weather_condition = weather_district[1]["time"]
     District_total_temperature = weather_district[2]["time"]
 
-    #Enter city code for specfic district
-    city_code = 9
     City_weather = response_City_j["records"]["locations"][0]["location"][district_code]["weatherElement"]
     City_total_weather_condition = City_weather[1]["time"]
     City_total_temperature = City_weather[2]["time"]
 
+    # Grab weather detail
     District_weather_list, District_str_weather, District_Bring_umbrella = grab_weather_details( District_total_weather_condition, District_total_temperature)
     City_weather_list, City_str_weather, City_Bring_umbrella = grab_weather_details( City_total_weather_condition, City_total_temperature)
-
+    # Store weather detail
     District_str_weather = store_weather_detail(District_weather_list, District_Bring_umbrella, District_str_weather)
     City_str_weather = store_weather_detail(City_weather_list, City_Bring_umbrella, City_str_weather)
 
+    # Print the result
     print(f"{response_NewTaipei_j_dis['records']['locations'][0]['location'][district_code]['locationName']}天氣預報\n{District_str_weather}")
     print(f"{response_City_j['records']['locations'][0]['location'][city_code]['locationName']}天氣預報\n{City_str_weather}")
 
+    # Send the weather alert by email
     m = EmailMessage()
-
     m['X-Priority'] = '1'
     m['Subject'] = "明日天氣預報"
     m.set_content(f"Hi,\n\n{response_NewTaipei_j_dis['records']['locations'][0]['location'][district_code]['locationName']}天氣預報\n{District_str_weather}\n\n{response_City_j['records']['locations'][0]['location'][city_code]['locationName']}天氣預報\n{City_str_weather}")
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()  ##Make secure
+        # Make secure
+        connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(my_email,
                             receiever,
